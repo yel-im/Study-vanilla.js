8f94df8c6ecb83e5bdab5a6540180b953a28f2a1
dPfla <choyr2016@naver.com>
2021-01-13 18:14:06 +0900

    [ADD] todolist js

---
+++ ToDo/ToDo.js
@@ -0,0 +1,25 @@
+document.getElementById('btnAdd').addEventListener('click', addList); //list 추가
+document.getElementById('atnDelAll').addEventListener('cilck', delAllEle); // list 전체삭제
+document.getElementById('atnDellAll').addEventListener('cilck', delLastEle); // 맨 마지막 요소 삭제
+document.getElementById('DeleteSel').addEventListener('click', delSelected); // 선택삭제
+
+// list 추가 (행 추가)
+function addList() {
+  var contents = document.querySelector('.text-basic');
+  
+}
+
+// list 전체삭제
+function delAllEle() {
+
+}
+
+// 맨 마지막 요소 삭제
+function delLastEle() {
+
+}
+
+// 선택삭제
+function delSelected() {
+
+}
